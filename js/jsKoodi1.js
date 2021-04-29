2d71610eaaa5e3367586e2fdaba3d55f7c7a7529
Janne Lahteenmaki <janne.lahteenmaki@metropolia.fi>
2021-04-29 12:35:58 +0300

    Kartan lisäksi sää boksi lisätty, molemmat voi kommentoida pois kunnes tarvitaan.

--- js/jsKoodi1.js
+++ js/jsKoodi1.js
@@ -1,11 +1,13 @@
 'use strict';
 
+//Kartan toiminta funktio
 function initMap() {
     let partyLocation = {lat: 60.167246, lng: 24.942074};
     let churchLocation = {lat: 60.172978, lng: 24.925196};
+    let centerLocation ={lat: 60.171435, lng: 24.931746 };
     let map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 15,
-        center: partyLocation
+        zoom: 14,
+        center: centerLocation
     });
     let partyMarker = new google.maps.Marker({
         position: partyLocation,
@@ -16,5 +18,16 @@ function initMap() {
         map: map
     });
 }
+
+//Sään toiminta funktio
+!function(d,s,id) {
+    let js,fjs=d.getElementsByTagName(s)[0];
+    if(!d.getElementById(id)) {
+        js=d.createElement(s);
+        js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';
+        fjs.parentNode.insertBefore(js,fjs);
+    }
+}
+(document,'script','weatherwidget-io-js');
 
 
