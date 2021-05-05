0cd75bbcfc8fb8fe11f15677d5ba82439d1b51df
sanku <san.kukkonen@gmail.com>
2021-05-05 13:01:54 +0300

    tässä on toimiva tuo rsvp nimihaku herukoapista. olkaa hyvät

--- js/rsvp.js
+++ js/rsvp.js
@@ -2,94 +2,53 @@
 // hakuosoitteen vakio-osa
 const apiurl = 'https://ryhmac.herokuapp.com/guests';
 
-// lopullinen hakukysely, joka lähetetään nettiin.
-let apiKysely;
 
 // Etsitään HTML-sivulta tarvittavat komponentit id:n avulla. (hakunappi, hakukenttä, main-elementti)
-const pwdbtn = document.getElementById('pwdbtn');
-const pwdspace = document.getElementById('pwd');
 const kn_article = document.getElementById('kutsuttujen_nimet');
+const typepwd = document.getElementById('typepwd'); // tekstikenttä, johon salasana syötetään
+const pwdbtn = document.getElementById('pwdbtn'); // pwd nappi
 
-const rsvpform = document.getElementById('form');
-
-function pwdSubmit(event) {
-  event.preventDefault();
-  apiKysely = apiurl;
-
-  kn_article.innerHTML= ``; //Tyhjennetään mainElementti ennen jokaista uutta tulostusta
-
-  /*teeHaku(apiKysely);
-
-   */
+function pwdSubmit(e) {
+  console.log('painoitnappia');
+  e.preventDefault();
+  teeHaku(apiurl);
 }
-rsvpform.addEventListener('submit', pwdSubmit());
+pwdbtn.addEventListener('click', pwdSubmit);
 
-/*
-pwdbtn.addEventListener('click', teeKysely);
-
-pwdspace.addEventListener('onkeyup', function(event) {
-  if(event.keyCode === 13) {
-    event.preventDefault();
-    document.getElementById("pwdbtn").click();
-  }
-});
-*/
-
-
-
-console.log('nappia on painettu');
-
-function teeKysely() {
-
-  //Muodostetaan URL-lähde
-  apiKysely = apiurl;
-
-  kn_article.innerHTML= ``; //Tyhjennetään mainElementti ennen jokaista uutta tulostusta
-
-  teeHaku(apiKysely);        // parametrina lähde
-}
-
-function teeHaku(apiKysely) {
+function teeHaku(apiurl) {
   // suoritetaan hakukysely, fetch hoitaa mahdolliset tietoliikenneongelmat.
-  fetch(apiKysely).then(function(response){
+
+  fetch(apiurl).then(function(response){
     return response.json();
   }).then(function(json) {
     naytaVastaus(json);				// siirrytään varsinaisen datan käsittelyyn.
   })};
 
 function naytaVastaus(jsonData) {
-  let salasana = pwdspace.value;
+kn_article.innerHTML=``;
+  let salasana = typepwd.value;
   for (let i = 0; i < jsonData.length; i++) {
     if (jsonData[i].password === salasana) {
       try {
-        kn_article.innerHTML= `<h3>Hei ${jsonData[i].fname} ${jsonData[i].lname} </h3>`;
+        if(jsonData[i].a_fname != null && jsonData[i].a_lname != null){
+          if(jsonData[i].lname === jsonData[i].a_lname){
+            kn_article.innerHTML += `<h3>Hei ${jsonData[i].fname} & ${jsonData[i].a_fname} ${jsonData[i].lname}!</h3>`;
+
+          }else{
+            kn_article.innerHTML += `<h3>Hei ${jsonData[i].fname} ${jsonData[i].lname} & ${jsonData[i].a_fname} ${jsonData[i].a_lname}!</h3>`;
+          }
+        }else {
+          kn_article.innerHTML += `<h3>Hei ${jsonData[i].fname} ${jsonData[i].lname}!</h3>`;
+        }
       } catch (e) {
-        console.log(jsonData[i].fname)
+        kn_article.innerHTML += `<p>Tarkistaisitteko salasananne</p>`;
       } break;
     } else {
       continue;
     }
   }
-  console.log(jsonData);
+  typepwd.style.display = "none"; // piiloitetaan typepwd
+  pwdbtn.style.display = "none"; // piiloitetaan pwdbtn
 }
 
-  /*try {
-    console.log(jsonData.guests[i].fname)
-  } catch (e){
-    console.log(jsonData.guests[i].fname)
-  }
 
-
-  kn_article.innerHTML=`<p>${jsonData[i].guests.fname}</p>`;
-try {
-        kn_article.innerHTML += `<h2>${jsonData.guests[i].fname}</h2><br>`
-      } catch (e) {
-        kn_article.innerHTML += `<h2>ei löytynyt</h2><br>`
-      }
-
-      break;
-    } else {
-      kn_article.innerHTML = `<p>nyteitoimi</p>`;
-    }
-
-}*/
